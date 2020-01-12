854e7b2b7225635482aecf4a43d5d78c0402efeb
arun <ayogeshwaran@vmware.com>
2020-01-12 19:11:46 +0530

    first commit

---
+++ app/src/main/java/com/buzz/vpn/Data.java
@@ -0,0 +1,15 @@
+package com.buzz.vpn;
+
+public class Data {
+    static String FileUsername = "alpha";
+    static String FilePassword = "682019";
+
+
+
+    static boolean isAppDetails = false, isConnectionDetails = false;
+    static String PREF_USAGE = "daily_usage";
+    public static String StringCountDown;
+    public static long LongDataUsage;
+
+
+}
